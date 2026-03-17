aaa8ad5161efb048b288115316eca1a1fde443ad
Peace Temadri <71979854+temadripeace@users.noreply.github.com>
2026-03-17 08:53:20 +0300

    Update geostand.py

--- geostand.py
+++ geostand.py
@@ -27,7 +27,7 @@ st.set_page_config(
 st.sidebar.markdown(
     """
     <div style="
-        font-size:13px;
+        font-size:12px;
         font-family:'Poppins', sans-serif;
         font-weight:500;
         line-height:2;
